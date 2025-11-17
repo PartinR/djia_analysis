aff4b3b8a772ccac0b0b6753bc7c149e21f6ed46
Race Partin <racepartin@gmail.com>
2025-11-17 15:52:04 -0500

    feat: add data folder and create basic fetch_data command

--- src/data.py
+++ src/data.py
@@ -3,4 +3,19 @@
 # Import statements
 import pandas as pd
 
+# File path to be read
+path = '/Users/racepartin/repos/djia_analysis/data/dow_jones_index.data'
+
 def fetch_data():
+    '''
+    Reads locally stored data using hardcoded path.
+    '''
+
+    try:
+        df = pd.read_csv(path)
+        print('Data successfully loaded.')
+        return df
+
+    except FileNotFoundError:
+        print('ERROR: File cannot be located. Check file path.')
+        return pd.DataFrame()
