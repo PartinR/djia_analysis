3f44fa65b0bfcb7d042ca9417e2e20c9fc6e6674
Race Partin <racepartin@gmail.com>
2025-11-19 19:58:09 -0500

    feat: add calculate_returns function for finding annualized returns

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,22 @@
 # djia_analysis/src/analysis.py
 
 # Import statements
+import pandas as pd
+import numpy as np
+
+def calculate_returns(df):
+    '''
+    Calculates the annualized return for each stock.
+    '''
+
+    # Sort and create weekly_return column using date and pct_change
+    df = df.sort_values(by=['stock', 'date'])
+    df['weekly_return'] = df.groupby('stock')['close'].pct_change()
+
+    # Calculate the mean of the weekly_return's
+    df_return = df.groupby('stock')['weekly_return'].mean().reset_index(name='avg_weekly_return')
+
+    # Annualize the weekly_return's
+    df_return['annualized_return'] = df_return['avg_weekly_return'] * 52
+
+    return df_return[['stock','annualized_return']]
