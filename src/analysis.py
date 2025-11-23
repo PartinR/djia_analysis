03c8df9eb0e6d4044606c4a48fdd6667b8baa17e
Race Partin <racepartin@gmail.com>
2025-11-22 19:50:59 -0500

    feat: implement calculate_std and calculate_sharpe_ratio

--- src/analysis.py
+++ src/analysis.py
@@ -4,12 +4,12 @@
 import pandas as pd
 import numpy as np
 
-def calculate_returns(df):
+def calculate_return(df):
     '''
     Calculates the annualized return for each stock.
     '''
 
-    if df.empty():
+    if df.empty:
         return df
 
     # Sort and create weekly_return column using date and pct_change
@@ -23,3 +23,37 @@ def calculate_returns(df):
     df_return['annualized_return'] = df_return['avg_weekly_return'] * 52
 
     return df_return[['stock','annualized_return']]
+
+def calculate_std(df):
+    '''
+    Calculate the annualized standard deviation for each stock.
+    '''
+
+    if df.empty:
+        return df
+
+     # Sort and create weekly_return column using date and pct_change
+    df = df.sort_values(by=['stock', 'date'])
+    df['weekly_return'] = df.groupby('stock')['close'].pct_change()
+
+    # Calculate the standard deviation of the weekly_return's
+    df_std = df.groupby('stock')['weekly_return'].std().reset_index(name='avg_weekly_std')
+
+    # Annualize the standard deviation
+    df_std['annualized_std'] = df_std['avg_weekly_std'] * np.sqrt(52)
+
+    return df_std[['stock','annualized_std']]
+
+def calculate_sharpe_ratio(df_return, df_std, risk_free_rate=0.02):
+    '''
+    Calculates the sharpe ratio for each stock.
+    Sharpe ratio = (annualized_return - risk_free_rate) / annualized_std
+    '''
+
+    # Merge df_returns and df_std
+    df_merge = pd.merge(df_return, df_std, on='stock', how='inner')
+
+    # Calculate the sharpe_ratio
+    df_merge['sharpe_ratio'] = (df_merge['annualized_return'] - risk_free_rate) / df_merge['annualized_std']
+
+    return df_merge[['stock', 'annualized_return', 'annualized_std', 'sharpe_ratio']]
